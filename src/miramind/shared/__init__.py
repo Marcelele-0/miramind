9217b8b042ba6cf338de3ce465e65acc07ff66ce
mmarsy <mikolaj.marsy@gmail.com>
2025-06-15 11:47:52 +0200

    feat: added yt downloader and shared package

---
+++ src/miramind/shared/__init__.py
@@ -0,0 +1,12 @@
+from openai import AzureOpenAI
+from dotenv import load_dotenv
+import os
+
+
+class MyClient:
+    @staticmethod
+    def get():
+        return AzureOpenAI(azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT_URL"],
+                           api_key=os.environ["AZURE_OPENAI_API_KEY"],
+                           api_version=os.environ["AZURE_OPENAI_API_VERSION"])
+    
