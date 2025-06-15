a7d66e452a37a73d28cac111a53a6627da5d6744
mmarsy <mikolaj.marsy@gmail.com>
2025-06-15 15:25:26 +0200

    feat: added transcribe method

--- src/miramind/stt/__init__.py
+++ src/miramind/stt/__init__.py
@@ -1,7 +1,16 @@
 from shared import MyClient
+from dotenv import load_dotenv
+import os
 
 
 class STT:
     def __init__(self):
         self.client = MyClient.get()
+
+    def transcribe(self, file):
+        load_dotenv()
+        with open(file, "rb") as audio_file:
+            transcription = self.client.audio.transcriptions.create(model=os.environ.get("STT_MODEL", "gpt-4o-transcribe"),
+                                                                    file=audio_file)
+            return transcription
         
