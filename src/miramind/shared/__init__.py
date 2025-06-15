4f064306aa5250d94b006acda542b8695dc42fcc
mmarsy <mikolaj.marsy@gmail.com>
2025-06-15 14:18:56 +0200

    feat: added cli downloader

--- src/miramind/shared/__init__.py
+++ src/miramind/shared/__init__.py
@@ -6,6 +6,7 @@ import os
 class MyClient:
     @staticmethod
     def get():
+        load_dotenv()
         return AzureOpenAI(azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT_URL"],
                            api_key=os.environ["AZURE_OPENAI_API_KEY"],
                            api_version=os.environ["AZURE_OPENAI_API_VERSION"])
