d98a7b68ef7e587263f0ee1ad0606e5ebce4447b
mmarsy <mikolaj.marsy@gmail.com>
2025-06-18 13:55:11 +0200

    feat: added threading

---
+++ src/miramind/stt/stt.py
@@ -0,0 +1,38 @@
+from shared import MyClient, S, A, U, msg
+from dotenv import load_dotenv
+import os
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+
+class STT:
+    client: any
+
+    def __init__(self):
+        self.client = MyClient.get()
+
+    def transcribe(self, file):
+        load_dotenv()
+        with open(file, "rb") as audio_file:
+            transcript = self.client.audio.transcriptions.create(model=os.environ.get("STT_DEPLOYMENT", "gpt-4o-transcribe"),
+                                                                 file=audio_file,
+                                                                 response_format="json",
+                                                                 timestamp_granularities=["word"])
+            
+        messages = [msg(S, "Detect language of the following text. Your answer should be one word. Example: english, polish, german."),
+                    msg(S, f"Text: {transcript.text}")]
+        response = self.client.chat.completions.create(model=os.environ.get("LANG_DETECT_DEPLOYMENT", "gpt-4o"),
+                                                       messages=messages,
+                                                       temperature=0.0)
+        
+        language = response.choices[0].message.content
+        return {"language": language,
+                "transcript": transcript.text}
+        
+
+if __name__ == "__main__":
+    load_dotenv()
+    my_stt = STT()
+    t = my_stt.transcribe_file(f"{os.environ["TESTS_DIR"]}/stt/rick_roll.m4a", response_format="json")
+    print(t)
+    
