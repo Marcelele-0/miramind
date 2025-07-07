79261a7595946d511c80ea86318ee975008ba721
mmarsy <mikolaj.marsy@gmail.com>
2025-07-07 13:04:15 +0200

    added listening thread

--- src/miramind/audio/stt/stt_class.py
+++ src/miramind/audio/stt/stt_class.py
@@ -1,6 +1,6 @@
+import logging
 import os
 
-from dotenv import load_dotenv
 from miramind.shared.utils import S, get_azure_openai_client, msg
 
 
@@ -26,14 +26,34 @@ class STT:
         client: client instance used for API calls.
     """
 
-    def __init__(self, client):
+    def __init__(self, client, logger=None):
         """
         Constructor of STT class.
 
         Args:
             client: client instance for API calls.
+            logger: logger instance for logging.
         """
         self.client = client
+        self.logger = logger if logger is not None else logging.getLogger()
+
+    def transcribe_bytes(self, bytes):
+        """
+        Transcribe bytes object.
+
+        Args:
+            bytes: bytes object representing sound to transcribe.
+
+        Returns:
+            dict[str: str]: dict containing transcript (with key "transcript")
+        """
+        transcript = self.client.audio.transcriptions.create(
+            model=os.environ.get("STT_DEPLOYMENT", "gpt-4o-transcribe"),
+            file=bytes,
+            response_format="json",
+        )
+        self.logger.info(f"Transcript: {transcript.text}")
+        return {"transcript": transcript.text}
 
     def transcribe(self, file_path: str) -> dict[str:str]:
         """
@@ -50,14 +70,13 @@ class STT:
         """
 
         with open(file_path, "rb") as audio_file:
-            transcript = self.client.audio.transcriptions.create(
-                model=os.environ.get("STT_DEPLOYMENT", "gpt-4o-transcribe"),
-                file=audio_file,
-                response_format="json",
-            )
+            return self.transcribe_bytes(audio_file)
 
         # TODO: decide if this part is necessary
         # response = self.client.chat.completions.create(model=os.environ.get("LANGUAGE_DETECTION_DEPLOYMENT", "04-mini"),
         #                                                messages=get_language_detection_prompt(transcript.text),)
 
-        return {"transcript": transcript.text}  # "language": response.choices[0].message.content,
+
+if __name__ == "__main__":
+    stt = STT(client=get_azure_openai_client())
+    print(stt.transcribe("C:/Users/Mikołaj/PythonProjects/miramind/test_output_sad.wav"))
